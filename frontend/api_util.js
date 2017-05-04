3331c0e95b5e34bac720dffd9124ea1451348f75
Elliot Humphrey <elliot.v.humphrey@gmail.com>
2017-05-03 18:01:35 -0700

    Update api_util to implicit return

--- frontend/api_util.js
+++ frontend/api_util.js
@@ -1,34 +1,31 @@
 const APIUtil = {
-  followUser: id => {
-    return $.ajax({
+  followUser: id => (
+    $.ajax({
       url: `/users/${id}/follow`,
       method: "POST",
       dataType: "JSON",
-      data: {
-        followee_id: id
-      }
-    });
-  },
-
-  unfollowUser: id => {
-    return $.ajax({
+      data: { followee_id: id }
+    })
+    ),
+  unfollowUser: id => (
+    $.ajax({
       url: `/users/${id}/follow`,
       method: "DELETE",
       dataType: "JSON",
       data: {
         followee_id: id
       },
-    });
-  },
+    })
+  ),
 
-  searchUsers: (queryVal) => {
-    return $.ajax({
+  searchUsers: (queryVal) => (
+    $.ajax({
       url: "/users/search",
       method: "GET",
       dataType: "JSON",
       data: queryVal,
-    });
-  }
+    })
+  )
 };
 
 
